eb3bb64f191e1eee9bd18fddc83ebeec294784f8
Nisar Ahmed <nisarbhutto180@gmail.com>
2025-07-06 23:52:32 +0500

    let,  const, and arrow functions

---
+++ Programming.js
@@ -0,0 +1,20 @@
+let car = []
+
+const cars = ["Saab", "Volvo", "BMW"];
+cars.push("bb", "Saab", "Volvo", "BMW")
+console.log(cars)
+
+
+function add() {
+    let a = 3;
+    let b = 5;
+    return a + b;
+}
+
+console.log(add())
+
+const c = (a, b) =>  a+b;
+
+
+
+console.log (c(39,89))
